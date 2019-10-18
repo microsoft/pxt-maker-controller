a940e1ea15ab732f8544a4c2a735fc3dd3243561
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-10-18 13:32:43 -0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 enum ControllerButton {
+    //% block="A"
     A = 0x01,
+    //% block="B"
     B = 0x02,
+    //% block="left"
     Left = 0x4,
+    //% block="up"
     Up = 0x08,
+    //% block="right"
     Right = 0x10,
+    //% block="down"
     Down = 0x20
 }
 
@@ -11,6 +17,8 @@ enum ControllerButton {
  * Blocks to build custom arcade controller
  */
 //% color=0xe95153 icon="\uf11b"
+//% weight=80
+//% blockNamespace="Maker Controller"
 namespace makerController {
     const NBUTTONS = 6;
     /**
@@ -20,53 +28,59 @@ namespace makerController {
     export class Player {
         private downs: number;
         constructor(public keys: string) {
-
+            this.downs = 0;
         }
 
         /**
-         * Sets a button state
+         * Simulate that the button has been pressed
          */
-        //% blockId=makercontrollerkey block="set %this %button to %event"
-        setButton(button: ControllerButton, event: KeyboardKeyEvent) {
+        //% blockId=makercontrollerpress block="press %this button %button"
+        press(button: ControllerButton) {
             button = button & 0xff;
-            switch (event) {
-                case KeyboardKeyEvent.Press:
-                    // clear downs
-                    this.downs = ~(~this.downs | button);
-                    // send press commands
-                    for (let i = 0; i < NBUTTONS; ++i) {
-                        keyboard.key(this.keys[i], KeyboardKeyEvent.Press)
-                    }
-                    break;
-                case KeyboardKeyEvent.Up:
-                    // send up commands
-                    const cdo = button & this.downs;
-                    for (let i = 0; i < NBUTTONS; ++i) {
-                        const ci = 1 << i;
-                        if (cdo & ci) // it's down
-                            keyboard.key(this.keys[i], KeyboardKeyEvent.Up)
-                    }
-                    // clear state
-                    this.downs = ~(~this.downs | button);
-                    break;
-                case KeyboardKeyEvent.Down:
-                    // send up commands
-                    const cup = button & ~this.downs;
-                    for (let i = 0; i < NBUTTONS; ++i) {
-                        const ci = 1 << i;
-                        if (cup & ci) // it's not down
-                            keyboard.key(this.keys[i], KeyboardKeyEvent.Down)
-                    }
-                    // clear state
-                    this.downs |= button;
-                    break;
+            // clear downs
+            this.downs = ~(~this.downs | button);
+            // send press commands
+            for (let i = 0; i < NBUTTONS; ++i) {
+                const ci = 1 << i;
+                if (button & ci)
+                    keyboard.key(this.keys[i], KeyboardKeyEvent.Press)
+            }
+        }
+
+        /**
+         * Sets a button state to down or up
+         */
+        //% blockId=makercontrollerkey block="set %this button %button to %down=toggleDownUp"
+        //% down.defl=true
+        setButtonDown(button: ControllerButton, down: boolean) {
+            button = button & 0xff;
+            if (down) {
+                // send up commands
+                const cup = button & ~this.downs;
+                for (let i = 0; i < NBUTTONS; ++i) {
+                    const ci = 1 << i;
+                    if (cup & ci) // it's not down
+                        keyboard.key(this.keys[i], KeyboardKeyEvent.Down)
+                }
+                // clear state
+                this.downs |= button;
+            } else {
+                // send up commands
+                const cdo = button & this.downs;
+                for (let i = 0; i < NBUTTONS; ++i) {
+                    const ci = 1 << i;
+                    if (cdo & ci) // it's down
+                        keyboard.key(this.keys[i], KeyboardKeyEvent.Up)
+                }
+                // clear state
+                this.downs = ~(~this.downs | button);
             }
         }
 
         /**
          * Reset the state of all controller buttons
          */
-        //% blockId=makercontrollerkey block="reset %this buttons"
+        //% blockId=makercontrollerreset block="reset %this buttons"
         reset() {
             for (let i = 0; i < NBUTTONS; ++i) {
                 const ci = 1 << i;
