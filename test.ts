afb5dac1857faaf03a0e39226172e0e970ec76fb
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-10-21 10:41:32 -0700

    fix test

--- test.ts
+++ test.ts
@@ -1,4 +1,5 @@
 // tests go here; this will not be compiled when this package is used as a library
+
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.A)
 })
@@ -8,8 +9,8 @@ input.buttonB.onEvent(ButtonEvent.Click, function () {
 input.buttonsAB.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.AB)
 })
-makerController.player1.setAnalogThreshold(ArcadeAnalogButton.A, 0, 200)
 forever(function () {
+    makerController.player1.setAnalogThreshold(ArcadeAnalogButton.A, 0, 200)
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
     makerController.player1.setAnalog(ArcadeAnalogButton.DownUp, input.acceleration(Dimension.Y))
     makerController.player1.setAnalog(ArcadeAnalogButton.A, input.soundLevel())
